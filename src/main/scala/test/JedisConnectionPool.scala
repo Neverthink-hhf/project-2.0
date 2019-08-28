ed82134caf3e2e487acdbb9adfc8513d6ddd64c9
Neverthink-hhf <979346466@qq.com>
2019-08-28 09:30:04 +0800

    Project-2.0

---
+++ src/main/scala/test/JedisConnectionPool.scala
@@ -0,0 +1,21 @@
+package test
+
+import redis.clients.jedis.{Jedis, JedisPool, JedisPoolConfig}
+
+/**
+  * 连接池
+  */
+object JedisConnectionPool {
+
+  val config = new JedisPoolConfig
+
+  config.setMaxTotal(30)// 最大连接
+
+  config.setMaxIdle(10)// 最大空闲
+
+  val pool = new JedisPool(config,"192.168.11.66",6379,10000)
+  // 获取Jedis对象
+  def getConnection():Jedis={
+    pool.getResource
+  }
+}
